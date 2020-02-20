d8ef6955d455e0c585335f53beac7069827a3489
jro0100 <jro0100@gmail.com>
2020-02-19 20:17:55 -0500

    lab 3, 4

--- Lab 2/src/edu/sussex/coms223/lab2/Map.java
+++ Lab 2/src/edu/sussex/coms223/lab2/Map.java
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+package edu.sussex.coms223.lab2;
+
+/**
+ * The Interface Map. Maintain a collection of values of generic type V that are
+ * looked up (mapped) by keys of generic type K.
+ * 
+ * Constraints:
+ * Neither a key nor value may be null.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
+ */
+public interface Map<K, V> {
+	V put(K key, V value);
+
+	V get(K key);
+
+	V remove(K key);
+
+	int size();
+
+	void clear();
+	
+	void print();
+}
+=======
 package edu.sussex.coms223.lab2;
 
 /**
@@ -21,3 +48,4 @@ public interface Map<K, V> {
 
 	void clear();
 }
+>>>>>>> e4a33cdd5481d9dea8affbc0e1c6c83177970e86
